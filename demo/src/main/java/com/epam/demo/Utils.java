431385982c32dce0eca8a45528f538d4edfe07dc
Nurbolat <nurba.dav@gmail.com>
2023-09-05 16:36:40 +0600

    attempt 3: added utils project as dependency in the demo project. Refactored isAllPositiveNumbers method to use StringUtils

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,14 +1,12 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
-        try {
-            return args.stream().map(Integer::parseInt).filter(i -> i > 0).count() == args.size();
-        } catch (NumberFormatException e) {
-            return false;
-        }
+        return args.stream().filter(StringUtils::isPositiveNumber).count() == args.size();
     }
 }
